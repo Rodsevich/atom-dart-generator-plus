d599b404cb4dd039141fbd5b33e1ba1f3921467c
Rodsevich <neton_12@hotmail.com>
2017-04-04 12:11:04 -0300

    init

---
+++ spec/atom-dart-generator-plus-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomDartGeneratorPlusView from '../lib/atom-dart-generator-plus-view';
+
+describe('AtomDartGeneratorPlusView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
